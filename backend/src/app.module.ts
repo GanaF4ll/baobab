860a7cd3a1cee38df4af8ee7c8ba70b5416bc9e7
GanaF4ll <ganafall9498@gmail.com>
2026-06-16 17:34:14 +0200

    feat: implement new documents module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { DrizzleModule } from './drizzle/drizzle.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DocumentsModule } from './documents/documents.module';
 
 @Module({
-  imports: [DrizzleModule, UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [DrizzleModule, UsersModule, AuthModule, ConfigModule.forRoot({ isGlobal: true }), DocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
